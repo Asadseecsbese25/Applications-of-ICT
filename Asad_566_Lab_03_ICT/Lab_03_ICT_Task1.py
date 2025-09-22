65796c650bac30cd165966b6d1517d2f11719aba
Asadseecsbese25 <masadrao426@gmail.com>
2025-09-22 09:05:38 -0700

    Add files via upload

---
+++ Asad_566_Lab_03_ICT/Lab_03_ICT_Task1.py
@@ -0,0 +1,8 @@
+name = "Asad"
+age = 18
+height = 6.1
+is_student = True
+print("my name is:",name)
+print("I am",age, "years old")
+print("I am" ,height, "inches tall")
+print("I am a Student:",is_student)
---
+++ Asad_566_Lab_03_ICT/Lab_03_ICT_Task2.py
@@ -0,0 +1,9 @@
+a= 10
+b= 15
+print("addition:",a+b)
+print("subtraction:",a-b)
+print("Multiplication:",a*b)
+print("division:",a/b)
+print("floor division",a//b)
+print("modulus",a%b)
+print("exponent:",a**b)
